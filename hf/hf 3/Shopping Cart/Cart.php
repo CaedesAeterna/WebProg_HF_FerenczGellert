fc2d8c18a7a29d9a6c9a39106637ec19f2da4cfc
CaedesAeterna <zoldcsirke0@gmail.com>
2023-10-11 20:37:36 +0300

    hf 3

---
+++ hf/hf 3/Shopping Cart/Cart.php
@@ -0,0 +1,93 @@
+<?php
+
+
+class Cart
+{
+    /**
+     * @var CartItem[]
+     */
+    private array $items = [];
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function setItems(array $items): void
+    {
+        $this->items = $items;
+    }
+
+    // TODO Generate getters and setters of properties
+
+
+    /**
+     * Add Product $product into cart. If product already exists inside cart
+     * it must update quantity.
+     * This must create CartItem and return CartItem from method
+     * Bonus: $quantity must not become more than whatever
+     * is $availableQuantity of the Product
+     *
+     * @param Product $product
+     * @param int $quantity
+     * @return CartItem
+     */
+    public function addProduct(Product $product, int $quantity): CartItem
+    {
+
+
+        $item = new CartItem($product, $quantity);
+
+        array_push($this->items, $item);
+
+        return $item;
+
+
+    }
+
+    /**
+     * Remove product from cart
+     *
+     * @param Product $product
+     */
+    public function removeProduct(Product $product): void
+    {
+        //TODO Implement method
+
+        foreach ($this->items as $key => $item) {
+            if ($item->getProduct()->getId() == $product->getId()) {
+                unset($this->items[$key]);
+            }
+        }
+
+    }
+
+    /**
+     * This returns total number of products added in cart
+     *
+     * @return int
+     */
+    public function getTotalQuantity(): int
+    {
+        $quantity = 0;
+        foreach ($this->items as $item) {
+            $quantity += $item->getQuantity();
+        }
+        return $quantity;
+
+    }
+
+    /**
+     * This returns total price of products added in cart
+     *
+     * @return float
+     */
+    public function getTotalSum(): float
+    {
+        $sum = 0;
+        foreach ($this->items as $item) {
+            $sum += $item->getProduct()->getPrice() * $item->getQuantity();
+        }
+        return $sum;
+    }
+}
---
+++ hf/hf 3/Shopping Cart/CartItem.php
@@ -0,0 +1,67 @@
+<?php
+
+
+class CartItem
+{
+    private Product $product;
+    private int $quantity;
+
+
+
+    /**
+     * @param Product $product
+     * @param int $quantity
+     */
+    public function __construct(Product $product, int $quantity)
+    {
+        $this->product = $product;
+        $this->quantity = $quantity;
+    }
+
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(Product $product): void
+    {
+        $this->product = $product;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+
+    public function increaseQuantity()
+    {
+        //TODO $quantity must be increased by one.
+        // Bonus: $quantity must not become more than whatever is Product::$availableQuantity
+        if ($this->quantity > $this->product->getAvailableQuantity()) {
+            $this->quantity = $this->product->getAvailableQuantity();
+        } else {
+            $this->quantity++;
+        }
+
+
+    }
+
+    public function decreaseQuantity()
+    {
+        //TODO $quantity must be increased by one.
+        // Bonus: Quantity must not become less than 1
+        if ($this->quantity < 1) {
+            $this->quantity = 1;
+        } else {
+            $this->quantity--;
+        }
+
+
+    }
+}
